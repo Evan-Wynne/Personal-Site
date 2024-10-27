67ad63b597b4fa91a7f3e6fa40dd66c3f828fe2b
Evan Wynne <Evan.Wynne@ssps.ie>
2024-10-27 19:02:24 +0000

    done

--- git.md
+++
@@ -1,6 +0,0 @@
-## How to make a git commit
-`
-git add . # Adds all the files in the directory (You don't need to do this if you haven't created any new files since the last commit)
-git commi -m "Your Message here"
-git push # Sends it to the remote repo
-`
--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('header');
-    const scrollPosition = window.scrollY;
-    const fadeStart = 100; // Start fading after 100px of scrolling
-    const fadeEnd = 300; // Fully faded at 300px of scrolling
-
-    if (scrollPosition <= fadeStart) {
-        header.style.opacity = 1;
-    } else if (scrollPosition >= fadeEnd) {
-        header.style.opacity = 0;
+/* JavaScript (script.js) */
+function openAccount() {
+    const email = document.getElementById('email').value;
+    if (email) {
+        alert(`Account opening process started for: ${email}`);
     } else {
-        const opacity = 1 - (scrollPosition - fadeStart) / (fadeEnd - fadeStart);
-        header.style.opacity = opacity;
+        alert('Please enter your email address');
     }
-});
+}
+
+function contactSales() {
+    alert('Contacting sales...');
+}
+
+
+
